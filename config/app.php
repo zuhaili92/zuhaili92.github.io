67f9711f3169643bd0b1b5b2e0c57077dd56b7f3
zuhaili92 <zuhaili.xperia@gmail.com>
2017-06-08 08:17:35 +0800

    Change URL

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://zuhaili92.herokuapp.com/'),
+    'url' => env('APP_URL', 'https://limitless-hollows-44660.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
