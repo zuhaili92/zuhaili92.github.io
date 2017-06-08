99e09d02e883a2b2c4900c6f9df2dc1f3dadc41f
zuhaili92 <zuhaili.xperia@gmail.com>
2017-06-08 08:46:53 +0800

    change file directory

--- config/app.php
+++ config/app.php
@@ -103,7 +103,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomStringSomeRandomString'),
+    'key' => env('APP_KEY', 'base64:WoPmJheZllWY56rGQGWvnLdWQFgFy7LyK62r87B0IzE='),
 
     'cipher' => 'AES-256-CBC',
 
